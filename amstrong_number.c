0f9929405a7dd1227accdfd3c8d985f6294f495f
DasAritra2001 <aritradas.stthomasit@gmail.com>
2022-04-09 20:24:48 +0530

    amstrong number

---
+++ amstrong_number.c
@@ -0,0 +1,20 @@
+// amstrong number or not 
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n , sum=0 , cube , i  ;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		cube=pow(i,3);
+		sum=sum+cube;
+	}
+	
+	if(sum==n)
+	{
+		printf("%d is amstrong ", n);
+	
+    }
+}
